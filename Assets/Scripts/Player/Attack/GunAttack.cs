654578f50c93aff57e4366ba49ed8aca83f4caf5
LeonardPannetier <leonard.pannetier@laposte.net>
2025-03-08 01:37:58 +0100

    l

--- Assets/Scripts/Player/Attack/GunAttack.cs
+++ Assets/Scripts/Player/Attack/GunAttack.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using Collision2D;
 
-public class GunAttack : MonoBehaviour
+public class GunAttack : PlayerAttack
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Bullet buletPrefabs;
+    [SerializeField] private Vector2 barrelOffset;
+
+    public override void SetPosition(Vector2 position)
     {
-        
+        base.SetPosition(position);
+        transform.position = position;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Launch()
     {
-        
+        base.Launch();
+
+        Bullet bullet = Instantiate(buletPrefabs, (Vector2)transform.position + barrelOffset, Quaternion.identity, transform);
+        Vector2 dir = (Vector2)Camera.main.ScreenToWorldPoint(InputManager.mousePosition) - (Vector2)transform.position;
+        bullet.Launch(dir, damage);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Circle.GizmosDraw((Vector2)transform.position + barrelOffset, 0.2f, Color.green);
     }
 }
--- Assets/Scripts/Player/Attack/PlayerAttack.cs
+++ Assets/Scripts/Player/Attack/PlayerAttack.cs
@@ -1,31 +1,35 @@
  using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
+public abstract class PlayerAttack : MonoBehaviour
 {
-    private float lastTimeLaunch = -10f;
+    private float lastTimeLaunch = float.MinValue;
 
-    [SerializeField] private InputKey activate;
     [SerializeField] private float cooldown;
+    [SerializeField] protected float damage;
 
-    private void Update()
+    protected virtual void Update()
     {
-        if(InputManager.GetKeyDown(activate))
-        {
-            TryLaunch();
-        }
+
     }
 
-    private void TryLaunch()
+    public bool TryLaunch()
     {
         if(Time.time - lastTimeLaunch >= cooldown)
         {
             Launch();
+            return true;
         }
+        return false;
+    }
+
+    public virtual void SetPosition(Vector2 position)
+    {
+
     }
 
     protected void OnTouchEnemy(EnemyController enemy)
     {
-
+        print($"Touch enemy:{enemy.gameObject.name}");
     }
 
     protected virtual void Launch()
